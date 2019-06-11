3ddbf5731e8c877078b46e88d023aac76ef0171b
weyheyhey <igokustov@gmail.com>
2019-06-12 02:06:53 +0300

    refactor(client): absolute to relative import

--- src/core/app.tsx
+++ src/core/app.tsx
@@ -1,9 +1,8 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-import { PageTemplate } from '@core/ui/templates';
-
 import { appRoutes } from './common/routes';
+import { PageTemplate } from './ui/templates';
 import { renderRouting } from './common/routing';
 
 import './assets/styles/global.scss';
